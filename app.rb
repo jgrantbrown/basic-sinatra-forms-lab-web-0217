496ab2f6be1060e1950c61185acf3ba2b11dc808
Your Name <you@example.com>
2017-02-20 00:17:24 -0500

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+  get '/newteam' do
+
+     erb :newteam#write your code here
+    end
+
+    post '/newteam' do
+      @name= params[:name]
+
+      @coach =  params[:coach]
+      @pg =[:pg]
+      @sg= [:sg]
+      @sf=  [:sf]
+      @pf=[:pf]
+      @c = [:c]
+      erb:team
+    end
 
 end
